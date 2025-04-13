605a8c52c292549e1ef8f6ec6750885598adce13
Prince <princemehta3382@gmail.com>
2025-04-13 14:19:28 +0530

    add error handling

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,26 @@
 const HTTP = require("../utils/httpStatus");
 
 const authCheckHandler = (req, res) => {
-  res.status(200).json({
-    code: HTTP.STATUS_200.CODE,
-    message: HTTP.STATUS_200.MESSAGE,
-    data: req.auth,
-  });
+  try {
+    if (!req.auth) {
+      return res.status(401).json({
+        code: HTTP.STATUS_401.CODE,
+        message: HTTP.STATUS_401.MESSAGE,
+      });
+    }
+
+    res.status(200).json({
+      code: HTTP.STATUS_200.CODE,
+      message: HTTP.STATUS_200.MESSAGE,
+      data: req.auth,
+    });
+  } catch (error) {
+    console.error("Auth Check Error:", error.message);
+    res.status(500).json({
+      code: HTTP.STATUS_500.CODE,
+      message: HTTP.STATUS_500.MESSAGE,
+    });
+  }
 };
 
 module.exports = {
